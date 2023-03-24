3d5c7319f1a5e6dee5c6f735b2bc3f46744cd218
buseph <76971029+buseph@users.noreply.github.com>
2023-03-25 00:01:46 +0800

    finished result summary component

--- pages/result-summary/index.js
+++ pages/result-summary/index.js
@@ -9,7 +9,7 @@ const data = [
 		category: "Reaction",
 		score: 80,
 		icon: "icon-reaction.svg",
-		bg: "bg-[#ff575715]",
+		bg: "bg-[#ff575710]",
 		color: "text-[#ff5757]",
 		alt: "icon-reaction",
 	},
@@ -17,7 +17,7 @@ const data = [
 		category: "Memory",
 		score: 92,
 		icon: "icon-memory.svg",
-		bg: "bg-[#ffb01f15]",
+		bg: "bg-[#ffb01f10]",
 		color: "text-[#ffb01f]",
 		alt: "icon-memory",
 	},
@@ -25,7 +25,7 @@ const data = [
 		category: "Verbal",
 		score: 61,
 		icon: "icon-verbal.svg",
-		bg: "bg-[#00bd9115]",
+		bg: "bg-[#00bd9110]",
 		color: "text-[#00bd91]",
 		alt: "icon-verbal",
 	},
@@ -33,7 +33,7 @@ const data = [
 		category: "Visual",
 		score: 72,
 		icon: "icon-visual.svg",
-		bg: "bg-[#1125d415]",
+		bg: "bg-[#1125d410]",
 		color: "text-[#1125d4]",
 		alt: "icon-visual",
 	},
@@ -41,54 +41,62 @@ const data = [
 
 export default function Page() {
 	return (
-		<main className={hanken.className}>
-			<div className="pt-5 pb-9 gap-6 flex flex-col items-center bg-gradient-to-b from-[#7857ff] to-[#2e2be9] rounded-b-3xl">
-				<h2 className="text-[#c8c7ff] font-medium text-base">Your Result</h2>
-				<div className="flex flex-col gap-2 justify-center rounded-full bg-gradient-to-b text-center from-[#4e21cac9] to-[#2421ca0a] w-40 h-40">
-					<h1 className="text-6xl font-bold text-[#fff]">76</h1>
-					<h6 className="text-base font-bold text-[#c8c7ff64]">of 100</h6>
+		<main className={hanken.className + " w-full h-screen sm:flex sm:flex-col sm:justify-center sm:items-center select-none"}>
+			<div className="flex flex-col sm:flex-row [&>*]:sm:basis-1/2 sm:shadow-2xl sm:shadow-[#1172d433] sm:rounded-3xl sm:w-full sm:max-w-[650px]">
+				<div className="pt-5 pb-9 sm:py-10 gap-6 sm:gap-8 flex flex-col items-center bg-gradient-to-b from-[#7857ff] to-[#2e2be9] rounded-b-3xl sm:rounded-3xl">
+					<h2 className="text-[#c8c7ff] font-medium text-base sm:text-xl">Your Result</h2>
+					<div className="flex flex-col gap-2 justify-center rounded-full bg-gradient-to-b text-center from-[#4e21ca] to-[#2421ca0f] w-40 h-40 sm:w-44 sm:h-44">
+						<h1 className="text-6xl font-bold text-[#fff]">76</h1>
+						<h6 className="text-base font-bold text-[#c8c7ff64] leading-tight">of 100</h6>
+					</div>
+					<div className="text-center">
+						<h2 className="mb-2 text-2xl font-bold text-[#fff] sm:text-[28px] sm:mb-3">Great</h2>
+						<p className="text-[#c8c7ffac] leading-tight text-base font-medium">
+							You scored higher than 65% of <br className="hidden sm:block" /> the <br className="sm:hidden" /> people who have taken <br className="hidden sm:block" /> these
+							tests.
+						</p>
+					</div>
 				</div>
-				<div className="text-center">
-					<h2 className="mb-2 text-2xl font-bold text-[#fff]">Great</h2>
-					<p className="text-[#c8c7ffac] leading-tight text-base font-medium">
-						You scored higher than 65% of the <br /> people who have taken these
-						tests.
-					</p>
-				</div>
-			</div>
 
-			<div className="px-6 pt-4 pb-8">
-				<h2 className="font-extrabold text-lg text-[#303b5a] mb-5">Summary</h2>
-				<div className="flex flex-col gap-4">
-					{data.map((item, index) => (
-						<div
-							key={index}
-							className={item.bg + " flex justify-between py-4 px-4 rounded-lg"}
-						>
-							<div className="flex">
-								<Image
-									className="w-5"
-									alt={item.alt}
-									src={`/result-summary/${item.icon}`}
-									width={200}
-									height={200}
-								/>
-								<h2 className={item.color + " ml-3 font-bold"}>
-									{item.category}
-								</h2>
+				<div className="px-6 pt-4 pb-8 sm:py-9 sm:px-9">
+					<h2 className="font-extrabold text-lg text-[#303b5a] mb-8 sm:text-[22px]">Summary</h2>
+					<div className="flex flex-col gap-4">
+						{data.map((item, index) => (
+							<div
+								key={index}
+								className={item.bg + " flex justify-between py-3 px-4 rounded-lg"}
+							>
+								<div className="flex">
+									<Image
+										className="w-5"
+										alt={item.alt}
+										src={`/result-summary/${item.icon}`}
+										width={200}
+										height={200}
+									/>
+									<h2 className={item.color + " ml-3 font-bold"}>
+										{item.category}
+									</h2>
+								</div>
+								<div>
+									<h2 className="font-extrabold text-[#303b5a80]">
+										<span className="text-[#303b5a]">{item.score}</span> / 100
+									</h2>
+								</div>
 							</div>
-							<div>
-								<h2 className="font-extrabold text-[#303b5a50]">
-									<span className="text-[#303b5a]">{item.score}</span> / 100
-								</h2>
-							</div>
-						</div>
-					))}
+						))}
+					</div>
+					<button className="bg-[#303b5a] mt-10 w-full text-base text-[#fff] font-bold py-3 rounded-3xl hover:bg-gradient-to-t from-[#2421ca] to-[#4e21ca]">
+						Continue
+					</button>
 				</div>
-				<button className="bg-[#303b5a] mt-7 w-full text-lg text-[#fff] font-bold py-3 rounded-3xl hover:bg-[#222c45] hover:text-[#ffffffcd] transition-all">
-					Continue
-				</button>
 			</div>
+			<p className='text-xs sm:text-sm pb-8 sm:pt-10 text-[#303b5a90] [&>*]:text-[#1125d4] [&>*]:opacity-70 text-center'>
+				Challenge provided by
+				<a title='This is an awesome website!' target="_blank" className='hover:font-semibold hover:opacity-100' href='https://www.frontendmentor.io'> Frontend Mentor</a>
+				. <br className='sm:hidden' />Site coded by
+				<a title='Wanna see my work? Click here!' target="_blank" className='hover:font-semibold hover:opacity-100' href='https://github.com/buseph'> jsph.dev</a>.
+			</p>
 		</main>
 	);
 }
