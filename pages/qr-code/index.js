8e2529dfa9c64cbae71f20df7ddf24b1afa34bb4
Joseph Barredo <76971029+buseph@users.noreply.github.com>
2024-10-12 21:49:17 +0800

    dynamic text for qr code page

--- pages/qr-code/index.js
+++ pages/qr-code/index.js
@@ -4,6 +4,8 @@ import qrCodePNG from '@public/qr-code/image-qr-code.png'
 import { Outfit } from 'next/font/google'
 import HeadSEO from '@components/HeadSEO'
 import Author from '@components/Author'
+// import the text from the text.js file
+import * as txt from './text'
 
 const outfit = Outfit({ subsets: ['latin'] })
 
@@ -13,16 +15,12 @@ export default function Page() {
 			<HeadSEO title="QR Code Component" image="qr-code" />
 			<div className={outfit.className + " flex flex-col items-center justify-center w-full h-screen font-outfit bg-[#d6e2f0]"}>
 				<div className='shadow-2xl shadow-[#2d7cfa4c] flex flex-col items-center p-4 text-center bg-[#fff] w-[300px] rounded-2xl text-[#1f3251]'>
-					<Image
-						src={qrCodePNG}
-						alt="Picture of the author"
-						className='rounded-xl'
-					/>
+					<Image src={qrCodePNG} alt="Picture of the author" className='rounded-xl' />
 					<h2 className='w-64 my-4 text-[22px] leading-tight font-bold'>
-						Improve your front-end skills by building projects
+						{txt.HEADER_TEXT}
 					</h2>
 					<p className='mb-5 text-[15px] w-[250px] leading-tight text-[#7b879d]'>
-						Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+						{txt.SUBHEADER_TEXT}
 					</p>
 				</div>
 				<Author />
---
+++ pages/qr-code/text.js
@@ -0,0 +1,5 @@
+// Text for the QRCode page
+const HEADER_TEXT = "Improve your front-end skills by building projects";
+const SUBHEADER_TEXT = "Scan the QR code to visit Frontend Mentor and take your coding skills to the next level";
+
+export { HEADER_TEXT, SUBHEADER_TEXT };
