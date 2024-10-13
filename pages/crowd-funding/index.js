3b3ae5ba31a79df23493a1b7d6fedbbb2a099c3e
Joseph Barredo <76971029+buseph@users.noreply.github.com>
2024-10-13 12:11:13 +0800

    1st section

--- pages/crowd-funding/index.js
+++ pages/crowd-funding/index.js
@@ -12,7 +12,7 @@ export default function Index() {
 	return (
 		<>
 			<HeadSEO title="Crowd Funding Landing Page" description="Crowd Funding Landing Page" image="crowd-funding-preview" />
-			<main className={commissioner.className}>
+			<main className={commissioner.className + " h-screen bg-[#fafafa]"}>
 				{/* Navbar with Background Image */}
 				<div className="relative h-[30rem] bg-[url('/crowd-funding/image-hero-desktop.jpg')] bg-center bg-cover">
 					{/* Navbar */}
@@ -31,6 +31,16 @@ export default function Index() {
 							</ul>
 						</nav>
 					</header>
+				</div>
+
+				{/* Main Content */}
+				<div className="grid place-items-center">
+					{/* First Section */}
+					<section className="absolute grid place-items-center w-[55vw] max-w-[66rem] rounded-lg border-[#f5f5f5] bg-white border-2 select-none">
+						<Image src="/crowd-funding/logo-mastercraft.svg" alt="Crowd Funding Master Craft Image" width={70} height={70} className="relative top-[-2.5rem]" />
+						<h1 className="text-4xl font-bold">{txt.FIRST_SECTION.title}</h1>
+						<p className="pt-5 text-xl font-light text-gray-400 pb-14">{txt.FIRST_SECTION.description}</p>
+					</section>
 				</div>
 				<Author />
 			</main>
